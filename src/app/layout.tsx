a7e484142a1d805e8c11bad4088c7f07584e65b7
Hema Vasanth <hemavasanth69@gmail.com>
2025-09-25 09:43:17 +0000

    the app is aligned unevenlly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import { Toaster } from "@/components/ui/toaster";
 import './globals.css';
 import { SidebarProvider } from "@/components/ui/sidebar";
@@ -16,7 +17,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&family=Source+Code+Pro&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased flex flex-col items-center min-h-screen">
+      <body className="font-body antialiased flex flex-col min-h-screen">
         <LanguageProvider>
           <UserProvider>
             <IssueProvider>
